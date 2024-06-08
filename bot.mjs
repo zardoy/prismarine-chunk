ac67e6e606d9398de6dbb5f53cc7aa074d9a2d90
Vitaly <vital2580@icloud.com>
2024-06-08 19:38:45 +0300

    simplify bot

--- bot.mjs
+++ bot.mjs
@@ -1,6 +1,7 @@
 //@ts-check
 import { createBot } from 'mineflayer'
 import { Vec3 } from 'vec3'
+import ChunkLoader from './src/index.js'
 
 const check = () => {
     const bot = createBot({
@@ -9,19 +10,29 @@ const check = () => {
       username: 'bot',
     })
 
+    const decodePacket = (packet, pos, expected) => {
+        const Chunk = ChunkLoader(bot.version)
+        //@ts-ignore
+        const chunk = new Chunk({ minY: bot.game.minY, worldHeight: bot.game.height })
+        chunk.load(packet.chunkData, packet.bitMap, true, true)
+        if (packet.biomes !== undefined) {
+            chunk.loadBiomes(packet.biomes)
+        }
+        if (packet.skyLight !== undefined) {
+            chunk.loadParsedLight(packet.skyLight, packet.blockLight, packet.skyLightMask, packet.blockLightMask, packet.emptySkyLightMask, packet.emptyBlockLightMask)
+        }
+        console.log(pos.toString(), chunk.getSkyLight(pos), expected)
+    }
+
     const pos1 = [15, 9, 21]
     const pos2 = [1, -63, 23]
     bot._client.on('map_chunk', (chunk) => {
         if (chunk.x === Math.floor(pos1[0] / 16) && chunk.z === Math.floor(pos1[2] / 16)) {
-            setTimeout(() => {
-                console.log('1', bot.world.getSkyLight(new Vec3(pos1[0], pos1[1], pos1[2]))) // expected 0
-            })
+            decodePacket(chunk, new Vec3(pos1[0], pos1[1], pos1[2]), 0)
         }
 
         if (chunk.x === Math.floor(pos2[0] / 16) && chunk.z === Math.floor(pos2[2] / 16)) {
-            setTimeout(() => {
-                console.log('2', bot.world.getSkyLight(new Vec3(pos2[0], pos2[1], pos2[2]))) // expected 15
-            })
+            decodePacket(chunk, new Vec3(pos2[0], pos2[1], pos2[2]), 15)
         }
     })
 }
