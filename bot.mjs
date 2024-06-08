50314e0e12a2f4bf75f936fe9e424ddfb691f19e
Vitaly <vital2580@icloud.com>
2024-06-08 19:53:43 +0300

    update code for ref against local save

--- bot.mjs
+++ bot.mjs
@@ -2,8 +2,14 @@
 import { createBot } from 'mineflayer'
 import { Vec3 } from 'vec3'
 import ChunkLoader from './src/index.js'
+import { Anvil as AnvilProvider } from 'prismarine-provider-anvil'
 
-const check = () => {
+const positionsCheck = [
+    [15, 9, 21, 2],
+    [1, -63, 23, 15],
+]
+
+const checkServer = () => {
     const bot = createBot({
       host: 'localhost',
     //   port: 25569,
@@ -24,17 +30,22 @@ const check = () => {
         console.log(pos.toString(), chunk.getSkyLight(pos), expected)
     }
 
-    const pos1 = [15, 9, 21]
-    const pos2 = [1, -63, 23]
     bot._client.on('map_chunk', (chunk) => {
-        if (chunk.x === Math.floor(pos1[0] / 16) && chunk.z === Math.floor(pos1[2] / 16)) {
-            decodePacket(chunk, new Vec3(pos1[0], pos1[1], pos1[2]), 0)
-        }
-
-        if (chunk.x === Math.floor(pos2[0] / 16) && chunk.z === Math.floor(pos2[2] / 16)) {
-            decodePacket(chunk, new Vec3(pos2[0], pos2[1], pos2[2]), 15)
+        const matchingPositions = positionsCheck.filter(([x, y, z]) => chunk.x === Math.floor(x / 16) && chunk.z === Math.floor(z / 16))
+        for (const [x, y, z, expected] of matchingPositions) {
+            decodePacket(chunk, new Vec3(x, y, z), expected)
         }
     })
 }
 
-check()
+const checkLocal = async () => {
+    const Anvil = AnvilProvider('1.18.2')
+    const world = new Anvil('./server/world/region')
+    for (const [x, y, z, expected] of positionsCheck) {
+        const chunk = await world.load(x >> 4, z >> 4) // returns the same "loaded" chunk
+        console.log(new Vec3(x, y, z).toString(), chunk.getSkyLight(new Vec3(x, y, z)), expected)
+    }
+}
+
+checkServer()
+// checkLocal()
